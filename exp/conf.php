222cf2ab80f19991a48fc956d2ff21667da3b62f
mkpappu <mkpappu1@gmail.com>
2024-01-16 16:11:15 -0500

    exp/conf.php works and is the same as v7.x

--- exp/conf.php
+++ exp/conf.php
@@ -1,51 +1,76 @@
 <?php
 
-//***********************************//
-//   EXPERIMENT CONFIGURATION FILE   //
+  //***********************************//
+ //   EXPERIMENT CONFIGURATION FILE   //
 //***********************************//
 
 // SET EXPERIMENT NAME
-$experimentName = 'Experiment Name';
-$experimentAlias = 'experiment_alias';
+$experimentName = 'Stroop Task';
+$experimentAlias = 'stroop';
 
 // SELECT LANGUAGE
 $language = 'english';
 // $language = 'french';
 // $language = 'german';
+?>
+<script type="text/javascript">
+    const language = "<?php echo $language; ?>";
+</script>
 
+<?php
 // SET SUBJECT IDENTIFICATION
-// MTurk case 
-if (isset($_GET["workerId"])) {
-  $workerId = $_GET["workerId"];
-  $subjectId = $_GET["workerId"];
-}
-// Prolific case
-if (isset($_GET["PROLIFIC_PID"])) {
-  $PROLIFIC_PID = $_GET["PROLIFIC_PID"];
-  $subjectId = $_GET["PROLIFIC_PID"];
-}
-// Connect case
-if (isset($_GET["participantId"])) {
-  $participantId = $_GET["participantId"];
-  $subjectId = $_GET["participantId"];
-}
+  if (isset($_GET["workerId"])) {
+  $workerId = isset($_GET["workerId"]) ? $_GET["workerId"] : null;
+  $subjectId = isset($_GET["workerId"]) ? $_GET["workerId"] : null;
+  } else {
+    $workerId = null;
+    $subjectId = null;
+  }
 
-if (isset($_GET["src_subject_id"])) {
-  $src_subject_id = $_GET["src_subject_id"];
-  $subjectId = $_GET["src_subject_id"];
-  
+  if (isset($_GET["PROLIFIC_PID"])) {
+  $PROLIFIC_PID = isset($_GET["PROLIFIC_PID"]) ? $_GET["PROLIFIC_PID"] : null;
+  $subjectId = isset($_GET["PROLIFIC_PID"]) ? $_GET["PROLIFIC_PID"] : null;
+  $workerId = null;
+  } else {
+    $PROLIFIC_PID = null;
+  }
+  if (isset($_GET["participantId"])) {
+  $participantId = isset($_GET["participantId"]) ? $_GET["participantId"] : null;
+  $subjectId = isset($_GET["participantId"]) ? $_GET["participantId"] : null;
+  $workerId = null;
+  } else {
+    $participantId = null;
+  }
+  if (isset($_GET["src_subject_id"])) {
+  $src_subject_id = isset($_GET["src_subject_id"]) ? $_GET["src_subject_id"] : null;
+  $subjectId = isset($_GET["src_subject_id"]) ? $_GET["src_subject_id"] : null;
   // these are omnibus data base variables which will get passed from participant portal
-  $studyId = $_GET["studyId"];
-  $candidateId = $_GET["candidateId"];
+  $studyId = isset($_GET["studyId"]) ? $_GET["studyId"] : null;
+  $candidateId = isset($_GET["candidateId"]) ? $_GET["candidateId"] : null;
   // these are NDA required variables which will get passed from participant portal
-  $subjectKey = $_GET["subjectkey"];
-  $consortId = $_GET["src_subject_id"];
-  $sexAtBirth = $_GET["sex"];
-  $institutionAlias = $_GET["site"];
-  $ageInMonths = $_GET["interview_age"];
-  $groupStatus = $_GET["phenotype"];
-  $visit = $_GET["visit"];
-}
+  $subjectKey = isset($_GET["subjectkey"]) ? $_GET["subjectkey"] : null;
+  $consortId = isset($_GET["src_subject_id"]) ? $_GET["src_subject_id"] : null;
+  $sexAtBirth = isset($_GET["sex"]) ? $_GET["sex"] : null;
+  $institutionAlias = isset($_GET["site"]) ? $_GET["site"] : null;
+  $ageInMonths = isset($_GET["interview_age"]) ? $_GET["interview_age"] : null;
+  $groupStatus = isset($_GET["phenotype"]) ? $_GET["phenotype"] : null;
+  $visit = isset($_GET["visit"]) ? $_GET["visit"] : null;
+  $workerId = null;
+  } else {
+    $src_subject_id = null;
+    $candidateId = null;
+    $studyId = null;
+    $subjectKey = null;
+    $consortId = null;
+    $sexAtBirth = null;
+    $institutionAlias = null;
+    $ageInMonths = null;
+    $groupStatus = null;
+    $visit = null;
+  }
+
+  
+
 
 /**
  * Get the hash of the current git HEAD
@@ -53,7 +78,7 @@ if (isset($_GET["src_subject_id"])) {
  * @return mixed Either the hash or a boolean false
  */
 
-function gitCommitHash( $branch='master' ) {
+ function gitCommitHash( $branch='master' ) {
   if ( $hash = file_get_contents( sprintf( '.git/refs/heads/%s', $branch ) ) ) {
     return "version: ".strval(substr(trim($hash),-7));
   } else {
@@ -65,31 +90,23 @@ function gitCommitHash( $branch='master' ) {
 <script type="text/javascript">
     const experimentName = "<?php echo $experimentName; ?>";
     const experimentAlias = "<?php echo $experimentAlias; ?>";
-    const language = "<?php echo $language; ?>";
+    const workerId = "<?php echo $workerId; ?>";
+    const PROLIFIC_PID = "<?php echo $PROLIFIC_PID; ?>";
+    const participantId = "<?php echo $participantId; ?>";
+    let src_subject_id = "<?php echo $src_subject_id; ?>";
+    let subjectId = "<?php echo $subjectId; ?>";
+
     const adminEmail = "joshua.kenney@yale.edu";
-    // these are NDA required variables which will get passed from participant portal
-    if (getParamFromURL("workerId")) {
-      const workerId = "<?php echo $workerId; ?>";
-      const subjectId = "<?php echo $workerId; ?>";
-    }
-    if (getParamFromURL("PROLIFIC_PID")) {
-      const PROLIFIC_PID = "<?php echo $PROLIFIC_PID; ?>";
-      const subjectId = "<?php echo $PROLIFIC_PID; ?>";
-    }
-    if (getParamFromURL("participantId")) {
-      const participantId = "<?php echo $participantId; ?>";
-      const subjectId = "<?php echo $participantId; ?>";
-    }
-    if (getParamFromURL("src_subject_id")) {
-      const src_subject_id = "<?php echo $src_subject_id; ?>";
-      const subjectId = "<?php echo $src_subject_id; ?>";
-      const GUID = "<?php echo $subjectKey?>";
-      const subjectID = "<?php echo $consortId?>";
-      const sexAtBirth = "<?php echo $sexAtBirth?>";
-      const siteNumber = "<?php echo $institutionAlias?>";
-      const ageAtAssessment = "<?php echo $ageInMonths?>";
-      const groupStatus = "<?php echo $groupStatus?>";
-      const visit = "<?php echo $visit?>";
-      const feedbackLink = "https://belieflab.yale.edu/omnibus/eCRFs/feedback/tasks/kamin.php?candidateId=<?php echo $candidateId?>&studyId=<?php echo $studyId?>";
-    }  
+    const feedbackLink = "https://belieflab.yale.edu/omnibus/eCRFs/feedback/tasks/kamin.php?candidateId=<?php echo $candidateId?>&studyId=<?php echo $studyId?>";
+    // these are NDA required variables which will get passed from participant portal 
+    const GUID = "<?php echo $subjectKey?>";
+    const subjectID = "<?php echo $consortId?>";
+    const sexAtBirth = "<?php echo $sexAtBirth?>";
+    const siteNumber = "<?php echo $institutionAlias?>";
+    const ageAtAssessment = "<?php echo $ageInMonths?>";
+    const groupStatus = "<?php echo $groupStatus?>";
+    const visit = "<?php echo $visit?>";
+
 </script>
+
+
